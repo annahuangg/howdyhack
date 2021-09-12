1385e591869e7a51db015e0e79adf0e57dc1c2ed
eric27n <73068837+eric27n@users.noreply.github.com>
2021-09-11 20:29:58 -0500

    Update kanye.py

--- kanye.py
+++ kanye.py
@@ -1,6 +1,6 @@
 import requests
 
-SPOTIFY_CREATE_PLAYLIST_URL = 'https://api.spotify.com/v1/users/am12012195/playlists'
+SPOTIFY_CREATE_PLAYLIST_URL = 'https://api.spotify.com/v1/users/bao2gq7wkdvblfemv1ge9jrxm/playlists'
 ACCESS_TOKEN = 'BQCnCM78HkELInUce2yZ42pyzprfRxFKXNhne7LMv5BZ4a12hMd26jLgutJ56FuKDR7EYhsuee2FFVdasSrB819SPgbmXd0KYqIA6gE0G3AigUfJWmzTFXc9p5RaHOo8wL1-epI7ZwyaUz13nk_ffZOkST2BZ8yti6Rv1hBGDkZKIej6Neu0ZmLrni_NMRaFb_BLH5qO'
 
 def create_playlist_on_spotify(name, public):
